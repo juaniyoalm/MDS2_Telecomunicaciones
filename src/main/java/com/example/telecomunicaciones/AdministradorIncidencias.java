23c82838eec2cbb939b10c5d5d987b8ca7c1621a
Juan <juanrdzbaeza@gmail.com>
2018-06-08 09:59:22 +0200

    Update AdministradorIncidencias.java

--- src/main/java/com/example/telecomunicaciones/AdministradorIncidencias.java
+++ src/main/java/com/example/telecomunicaciones/AdministradorIncidencias.java
@@ -1,10 +1,34 @@
 package com.example.telecomunicaciones;
 
+import java.util.ArrayList;
+
+import com.example.telecomunicaciones.bd.BD_Principal;
+import com.example.telecomunicaciones.bd.IAdministrador;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.renderers.ButtonRenderer;
 
 public class AdministradorIncidencias extends V_AdministradorIncidencias {
+	
+	IAdministrador administradorBD;
+	
 	public AdministradorIncidencias() {
+		Inicializar();
+	}
+	
+	private void Inicializar() {
+		
+		administradorBD = new BD_Principal();
+		
+		ArrayList<com.example.telecomunicaciones.bd.orm.Incidencia> incidencias = administradorBD.cargarIncidencias();
+		Grid<com.example.telecomunicaciones.bd.orm.Incidencia> grid = new Grid<>();
+		grid.setItems(incidencias);
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Incidencia::getID).setCaption("ID");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Incidencia::getAsunto).setCaption("Asunto");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Incidencia::getDescripcion).setCaption("Descripcion");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Incidencia::getFecha).setCaption("Fecha");
+
+		
 		tableIncidenciasS.addColumn(person -> "Ver",
 			      new ButtonRenderer(clickEvent -> {
 			    		ComercialDetalleIncidencia di = new ComercialDetalleIncidencia();
