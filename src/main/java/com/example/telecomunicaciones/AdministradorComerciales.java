cb9389fa94392639852409619b7c6d5e1782d154
Juan <juanrdzbaeza@gmail.com>
2018-06-08 09:13:44 +0200

    Update AdministradorComerciales.java

--- src/main/java/com/example/telecomunicaciones/AdministradorComerciales.java
+++ src/main/java/com/example/telecomunicaciones/AdministradorComerciales.java
@@ -1,18 +1,41 @@
 package com.example.telecomunicaciones;
 
+import java.util.ArrayList;
+
+import com.example.telecomunicaciones.bd.BD_Principal;
+import com.example.telecomunicaciones.bd.IAdministrador;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.PopupView;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.renderers.ButtonRenderer;
 
 public class AdministradorComerciales extends V_AdministradorComerciales {
+	
+	IAdministrador administradorBD;
 
 	public AdministradorComerciales() {
+		Inicializar();
+	}
+	
+	private void Inicializar() {
 		
+		administradorBD = new BD_Principal();
+	
 		AdministradorNew_ModComercial nc = new AdministradorNew_ModComercial();
 		PopupView pv = new PopupView(null, nc);
 		pv.setStyleName("pNuevoCliente");
 		verticalPanel.addComponent(pv);
 		
+		// Crea una tabla a traves de la lista de clientes
+		ArrayList<com.example.telecomunicaciones.bd.orm.Comercial> comerciales = administradorBD.cargarComerciales();
+		Grid<com.example.telecomunicaciones.bd.orm.Comercial> grid = new Grid<>();
+		grid.setItems(comerciales);
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Comercial::getIDComercial).setCaption("ID");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Comercial::getNombre).setCaption("Nombre");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Comercial::getApellido1).setCaption("Apellido 1");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Comercial::getApellido2).setCaption("Apellido 2");
+		grid.addColumn(com.example.telecomunicaciones.bd.orm.Comercial::getNif).setCaption("DNI");
+				
 		tablaComerciales.addColumn(person -> "Ver",
 			      new ButtonRenderer(clickEvent -> {
 			    		AdministradorDetalleComercial dc = new AdministradorDetalleComercial();
